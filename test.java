13601d4fa7c75343f038ad3c6bae3511aca4b8b2
unknown <premsingh192002@gmail.com>
2024-10-30 22:39:19 +0530

    gcd

--- test.java
+++ test.java
@@ -1,23 +1,22 @@
 import java.util.*;
 class test {
-    public static void main(String[] args) {
-        int range = 12;
-        boolean[] arr=new boolean[range];
-        arr[0]=false;
-        arr[1]=false;
-        for(int j=2;j*j<range;j++){
-            for(int i=j;i<=(arr.length)/j;i++){
-
-                arr[(i*j)-1]=true;
-            }
-    }
-    int count =0;
-    for(int i=1;i<arr.length;i++){
-        if(arr[i]==false){
-            count++;
+    static int gcd(int a, int b){
+        if(b==0){
+            return a;
         }
+        return gcd(b,a%b);
     }
+    public static void main(String[] args) {
+    // gcd of 2 numbers
+    int a = 10;
+    int b= 25;
 
-    System.out.println(count);
+    test obj = new test();
+    System.out.println(obj.gcd(a,b));
+
+
+
+   
+
     }
 }
